76e198100fae0bfc1ac88d3b6ca4b27fc4a764b6
felixfilipi <felixfilipi@gmail.com>
2022-05-09 17:00:21 +0700

    Complete Development || Fix Responsive || Status Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     backgroundImage: {
       background: "url('../assets/background.jpg')",
       dashboard: "url('../assets/dashboard.jpeg')",
+      working: "url('../assets/working.jpeg')",
+      award: "url('../assets/award.jpg')",
+      publication: "url('../assets/publication.jpeg')",
+      project: "url('../assets/project.jpeg')",
     },
   },
   plugins: [],
